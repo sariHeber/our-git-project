88f19e1c96eb47f188c2ebc30587a2340ba97408
Hanna_attali <Hannaattali2004@gmail.com>
2024-02-13 15:18:38 +0200

    תיקון שני

--- our-project/src/components/entry.jsx
+++ our-project/src/components/entry.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Sari } from './ourComponents/sari'
+import { Hanna } from './ourComponents/hanna'
 
 export const Entry = () => {
 
     return (
-        <>
-        <p>holle!!!</p>
-        <p>i am hanna attali</p>
-        <p>i love you my friends</p>
-        <Sari></Sari>
-        </>
+        <div>
+            <p>holle!!!</p>
+            <p>i am hanna attali</p>
+            <p>i love you my friends</p>
+            <Sari></Sari>
+            <Hanna></Hanna>
+        </div>
+       
     )
 }
--- our-project/src/components/ourComponents/hanna.jsx
+++ our-project/src/components/ourComponents/hanna.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export const Sari = () => {
+export const Hanna = () => {
     return (
        <div style={{borderColor: 'blue', borderStyle:'double'}}>
         <p>name: hanna</p>
