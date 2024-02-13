6ed52ba1e1716763f7130021addf6472a468ac0d
sariHeber <sa0548503880@gmail.com>
2024-02-13 14:41:01 +0200

    add entry & sari components

---
+++ our-project/src/components/entry.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Sari } from './ourComponents/sari'
+
+export const Entry = () => {
+
+    return (
+        <>
+        <p>holle!!!</p>
+        <Sari></Sari>
+        </>
+    )
+}
---
+++ our-project/src/components/ourComponents/sari.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Sari = () => {
+    return (
+       <div style={{borderColor: 'green', borderStyle:'double'}}>
+        <p>name: sari</p>
+        <p>age: 22</p>
+        <p>phone: 0548503880</p>
+        <p>email: sa0548503880@gmail.com</p>
+       </div>
+    )
+}
