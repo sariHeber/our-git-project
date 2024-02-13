56ca840e2a052a5b92e91243c414bbc8037475a3
Hanna_attali <Hannaattali2004@gmail.com>
2024-02-13 15:12:06 +0200

    תיקון

---
+++ our-project/src/components/ourComponents/hanna.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Sari = () => {
+    return (
+       <div style={{borderColor: 'blue', borderStyle:'double'}}>
+        <p>name: hanna</p>
+        <p>age: 19</p>
+        <p>phone: 0556750507</p>
+        <p>email: hannaattali2004@gmail.com</p>
+        </div>
+    )
+}
